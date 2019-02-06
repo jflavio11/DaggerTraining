53cb6b7d4d7c0359867420564643dda73c141c68
Jose Quispe <jose.quispe@avantica.net>
2019-02-06 12:48:27 -0500

    add keyboard to login activity

--- app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
+++ app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.jflavio1.daggerexample.login;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 import com.jflavio1.daggerexample.R;
+import com.jflavio1.daggerexample.core.VKeyboard;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText editText = findViewById(R.id.loginActivity_et);
+        VKeyboard vKeyboard = findViewById(R.id.loginActivity_keyboard);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        vKeyboard.setInputConnection(ic);
+
     }
 }
