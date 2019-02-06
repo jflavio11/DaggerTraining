948b51e57f30991766e27fe549e4d2de33f2f93c
JoseFlavio <jflavio90@gmail.com>
2019-02-06 10:31:12 -0500

    add login activity

---
+++ app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.jflavio1.daggerexample.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jflavio1.daggerexample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
