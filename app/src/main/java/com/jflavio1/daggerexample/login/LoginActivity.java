43c6b0ac62b9897eb807172eb5464fcd35cc3b01
Jose Quispe <jose.quispe@avantica.net>
2019-02-07 18:11:00 -0500

    update login activity for password like BCP

--- app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
+++ app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
@@ -5,22 +5,51 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import com.jflavio1.daggerexample.R;
 import com.jflavio1.daggerexample.core.components.keyboard.CustomKeyboardView;
+import com.jflavio1.daggerexample.core.components.keyboard.KeyboardListener;
+import com.jflavio1.daggerexample.core.components.keyboard.controller.KeyboardController;
 
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
     private CustomKeyboardView keyboardView;
+    private EditText editText;
+    private EditText editText2;
+    private EditText editText3;
+    private EditText editText4;
+    private EditText editText5;
+    private EditText editText6;
+
+    private EditText[] passFields = new EditText[6];
+
+
+    private int currentEditTextPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText editText = findViewById(R.id.loginActivity_et);
+        // TODO: edittexts must be a composition of unique custom view
+        // for checking when click onto the custom view (the viewgroup)
+        // is done and then start typing (like BCP app)
+        editText = findViewById(R.id.loginActivity_et_pass1);
+        editText2 = findViewById(R.id.loginActivity_et_pass2);
+        editText3 = findViewById(R.id.loginActivity_et_pass3);
+        editText4 = findViewById(R.id.loginActivity_et_pass4);
+        editText5 = findViewById(R.id.loginActivity_et_pass5);
+        editText6 = findViewById(R.id.loginActivity_et_pass6);
+
+        passFields[0] = editText;
+        passFields[1] = editText2;
+        passFields[2] = editText3;
+        passFields[3] = editText4;
+        passFields[4] = editText5;
+        passFields[5] = editText6;
 
         keyboardView = findViewById(R.id.loginActivity_keyboard);
 
+        // TODO get key values from server/file
         ArrayList<String> serverKeyValues = new ArrayList<>();
         serverKeyValues.add("1");
         serverKeyValues.add("3");
@@ -36,17 +65,40 @@ public class LoginActivity extends AppCompatActivity {
         CustomKeyboardView.KeyboardType type = CustomKeyboardView.KeyboardType.BANK_PASSWORD;
         type.setServerKeyValues(serverKeyValues);
 
-        keyboardView.registerEditText(type, editText);
+        keyboardView.setListener(new KeyboardListener() {
+            @Override
+            public void characterClicked(char c) {
+                if (currentEditTextPosition < 5) {
+                    currentEditTextPosition++;
+                    passFields[currentEditTextPosition].requestFocus();
+                } else {
+                    keyboardView.translateLayout();
+                }
+            }
 
+            @Override
+            public void specialKeyClicked(KeyboardController.SpecialKey key) {
+                if (key == KeyboardController.SpecialKey.DELETE_ALL) {
+                    for (EditText passField : passFields) {
+                        passField.clearFocus();
+                        passField.getText().clear();
+                    }
+                    currentEditTextPosition = 0;
+                }
+            }
+        });
+
+        keyboardView.registerEditText(type, editText);
+        keyboardView.registerEditText(type, editText2);
+        keyboardView.registerEditText(type, editText3);
+        keyboardView.registerEditText(type, editText4);
+        keyboardView.registerEditText(type, editText5);
+        keyboardView.registerEditText(type, editText6);
     }
 
     @Override
     public void onBackPressed() {
-        if (keyboardView.isExpanded()) {
-            keyboardView.translateLayout();
-        } else {
-            super.onBackPressed();
-        }
+
     }
 
 }
