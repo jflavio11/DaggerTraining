73165bd9fdfc813d3ba214ce3fcbbce11c2637fe
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2019-02-08 16:10:22 -0500

    CountDown timer for token generation

--- app/src/main/java/com/jflavio1/daggerexample/generateOtp/ui/TokenActivity.java
+++ app/src/main/java/com/jflavio1/daggerexample/generateOtp/ui/TokenActivity.java
@@ -1,6 +1,8 @@
 package com.jflavio1.daggerexample.generateOtp.ui;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,8 +14,8 @@ import com.jflavio1.daggerexample.generateOtp.view.TokenGeneratorView;
 
 public class TokenActivity extends BaseActivity implements TokenGeneratorView {
 
-    Button materialButton;
     TextView appCompatTextView;
+    CountDownTimer countDownTimer;
 
     private TokenGeneratorPresenterImpl presenter = new TokenGeneratorPresenterImpl();
 
@@ -27,9 +29,18 @@ public class TokenActivity extends BaseActivity implements TokenGeneratorView {
     }
 
     void initUI(){
-        materialButton = findViewById(R.id.btn_generate_token);
         appCompatTextView = findViewById(R.id.txt_token);
-        materialButton.setOnClickListener(v -> presenter.generateNewOtpToken());
+        countDownTimer = new CountDownTimer(60000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
     }
 
     @Override
@@ -46,5 +57,6 @@ public class TokenActivity extends BaseActivity implements TokenGeneratorView {
     protected void onDestroy() {
         super.onDestroy();
         this.presenter.onViewDestroyed();
+        countDownTimer.cancel();
     }
 }
