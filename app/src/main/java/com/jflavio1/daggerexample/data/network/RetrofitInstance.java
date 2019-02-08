fd599efad90e1b76217aa26bbb612446759a3e08
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2019-02-08 12:02:01 -0500

    Key store for versions

--- app/src/main/java/com/jflavio1/daggerexample/data/network/RetrofitInstance.java
+++ app/src/main/java/com/jflavio1/daggerexample/data/network/RetrofitInstance.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = BuildConfig.ApiKey;
+    private static final String BASE_URL = BuildConfig.BASE_URL;
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
