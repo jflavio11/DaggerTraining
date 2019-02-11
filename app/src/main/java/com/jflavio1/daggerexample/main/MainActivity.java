8f2f03d87a08f25b307ce2a9a9a6786db2e9bed8
enzoftware <lizama.enzo@gmail.com>
2019-02-10 20:48:23 -0500

    Create activity to move around POCs

---
+++ app/src/main/java/com/jflavio1/daggerexample/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jflavio1.daggerexample.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jflavio1.daggerexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
