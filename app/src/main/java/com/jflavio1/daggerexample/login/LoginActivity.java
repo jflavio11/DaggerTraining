54f62b6edd0e1ec0a8e6e45bc1b22b53b8cff606
Jose Quispe <jose.quispe@avantica.net>
2019-02-11 12:08:26 -0500

    add PasswordTextInput

--- app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
+++ app/src/main/java/com/jflavio1/daggerexample/login/LoginActivity.java
@@ -3,13 +3,25 @@ package com.jflavio1.daggerexample.login;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.jflavio1.daggerexample.R;
+import com.jflavio1.daggerexample.core.components.keyboard.CustomKeyboardView;
+import com.jflavio1.daggerexample.core.components.passwordTextInput.PasswordTextInput;
+import com.jflavio1.daggerexample.data.network.LocalKeyboardRepositoryImpl;
 
-public final class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class LoginActivity extends AppCompatActivity implements LoginView {
+
+    LoginPresenter presenter = new LoginPresenterImpl();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        presenter.injectView(this);
+        presenter.injectKeyboardRepository(new LocalKeyboardRepositoryImpl());
+        presenter.loadServerKeyboardPosition();
 
     }
 
@@ -18,4 +30,10 @@ public final class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onKeyboardPositionsLoaded(List<String> positions) {
+        CustomKeyboardView keyboardView = findViewById(R.id.loginActivity_keyboard);
+        PasswordTextInput passwordTextInput = findViewById(R.id.loginActivity_pti);
+        passwordTextInput.setKeyboardView(keyboardView, (ArrayList) positions);
+    }
 }
