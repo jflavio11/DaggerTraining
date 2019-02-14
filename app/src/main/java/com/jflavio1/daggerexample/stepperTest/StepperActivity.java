0b25bc14ef623d5d40080f58e84660a4c9e09614
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2019-02-14 16:37:49 -0500

    Fix : Reset horizontal stepper

--- app/src/main/java/com/jflavio1/daggerexample/stepperTest/StepperActivity.java
+++ app/src/main/java/com/jflavio1/daggerexample/stepperTest/StepperActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,9 +12,10 @@ import com.jflavio1.daggerexample.R;
 import com.jflavio1.daggerexample.core.components.stepper.NavigationBar;
 import com.jflavio1.daggerexample.core.components.stepper.NavigationTab;
 
-public class StepperActivity extends AppCompatActivity implements NavigationBar.OnTabSelected, NavigationBar.OnTabClick {
+public class StepperActivity extends AppCompatActivity implements NavigationBar.OnTabSelected{
 
     private NavigationBar bar;
+    private Button btnReset, btnNext, btnPrevious, btnSubmit;
     private int position = 0;
     private EditText etNumberOfCount;
 
@@ -22,48 +24,39 @@ public class StepperActivity extends AppCompatActivity implements NavigationBar.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stepper);
-        etNumberOfCount = (EditText) findViewById(R.id.et_count);
-        bar = (NavigationBar) findViewById(R.id.navBar);
+        etNumberOfCount = findViewById(R.id.et_count);
+        btnNext = findViewById(R.id.btn_next);
+        btnPrevious = findViewById(R.id.btn_prev);
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnReset = findViewById(R.id.btn_reset);
+        bar = findViewById(R.id.navBar);
         bar.setOnTabSelected(this);
-        bar.setOnTabClick(this);
-        setup(true, 4);
+        setup(false, 4);
+
+        btnReset.setOnClickListener(v->{
+            setup(true, 4);
+            etNumberOfCount.setText("");
+        });
+
+        btnNext.setOnClickListener(v-> bar.setCurrentPosition(++position));
+        btnPrevious.setOnClickListener(v-> bar.setCurrentPosition(--position));
+
+        btnSubmit.setOnClickListener(v-> setup(true, Integer.parseInt(etNumberOfCount.getText().toString())));
     }
 
     private void setup(boolean reset, int count) {
-        if (reset)
+        if (reset){
             bar.resetItems();
+            position = 0;
+        }
         bar.setTabCount(count);
         bar.animateView(3000);
         bar.setCurrentPosition(position <= 0 ? 0 : position);
     }
 
-    public void onClickView(View v) {
-        switch (v.getId()) {
-            case R.id.btn_reset:
-                setup(true, 4);
-                etNumberOfCount.setText("");
-                break;
-            case R.id.btn_next:
-                bar.setCurrentPosition(++position);
-                break;
-            case R.id.btn_prev:
-                bar.setCurrentPosition(--position);
-                break;
-            case R.id.btn_submit:
-                setup(false, Integer.parseInt(etNumberOfCount.getText().toString()));
-                break;
-        }
-    }
-
-    @Override
-    public void onTabClick(int touchPosition, NavigationTab prev, NavigationTab NavigationTab) {
-        Toast.makeText(getApplicationContext(), "You clicked on: " + touchPosition, Toast.LENGTH_LONG).show();
-
-    }
 
     @Override
     public void onTabSelected(int touchPosition, NavigationTab prev, NavigationTab NavigationTab) {
-        Toast.makeText(getApplicationContext(), "Selected position: " + touchPosition, Toast.LENGTH_LONG).show();
-
+        Toast.makeText(this, "Selected position: " + touchPosition, Toast.LENGTH_LONG).show();
     }
 }
